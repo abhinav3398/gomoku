05feef8000853a2dba02ff6750b6064568e25d40
Abhinav Lakhani <3398abhinav@gmail.com>
2022-06-21 09:56:21 -0400

    initialize game state

--- gomoku_cli.py
+++ gomoku_cli.py
@@ -1,7 +1,26 @@
 """
 Gomoku Player
 """
+import numpy as np
 
 X = 1 # White Player
 O = -1 # Black Player
 EMPTY = 0
+
+def initial_state(size=15):
+    """
+    Returns starting state of the board.
+    """
+    return np.array([[EMPTY]*size]*size)
+
+#----------------------------------------------------------------------
+# main game
+#----------------------------------------------------------------------
+def gamemain():
+    board_size = 15
+    board = initial_state(board_size)
+
+#----------------------------------------------------------------------
+if __name__ == '__main__':
+    gamemain()
+
